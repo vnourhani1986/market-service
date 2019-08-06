3ed3763bca225dded165dfc5ca292de4995b1878
vahid nourhani <vnourhani1986@gmail.com>
2019-08-06 13:12:13 +0430

    change email to lower case

--- src/main/scala/com/snapptrip/utils/formatters/EmailFormatter.scala
+++ src/main/scala/com/snapptrip/utils/formatters/EmailFormatter.scala
@@ -4,11 +4,11 @@ object EmailFormatter {
 
   def format(email: String): String = {
     val e = email.split("@").toList
-    if(e.last == "gmail.com") {
+    (if(e.last == "gmail.com") {
       e.head.replace(".", "") + "@" + e.last
     } else {
       e.head + "@" + e.last
-    }
+    }).toLowerCase
   }
 
 }
