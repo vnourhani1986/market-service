064e0bec52af1a22b47c22794e7421ea080139bc
Mehdi Hamzehlou <hamzehlou.mehdi@gmail.com>
2020-01-27 19:16:07 +0330

    fixed bug sqlinjection and check validation mobile and email

--- src/main/scala/com/snapptrip/utils/formatters/MobileNoFormatter.scala
+++ src/main/scala/com/snapptrip/utils/formatters/MobileNoFormatter.scala
@@ -1,6 +1,24 @@
 package com.snapptrip.utils.formatters
 
+import scala.util.Try
+
 object MobileNoFormatter {
+
+  def isShort(aString: String): Boolean = Try(aString.toLong).isSuccess
+
+  def isInt(aString: String): Boolean = Try(aString.toInt).isSuccess
+
+  def isLong(aString: String): Boolean = Try(aString.toLong).isSuccess
+
+  def isDouble(aString: String): Boolean = Try(aString.toDouble).isSuccess
+
+  def isFloat(aString: String): Boolean = Try(aString.toFloat).isSuccess
+
+  def isNumber(x: String): Boolean = {
+    val result = noPersianToEnglish(x.trim)
+    List(isLong(result))
+      .foldLeft(false)(_ || _)
+  }
 
   val formats = List(("""[0][9][0-9][0-9]{8,8}""", 1), ("""[0][0][9][8][9][0-9][0-9]{8,8}""", 4), ("""[+][9][8][9][0-9][0-9]{8,8}""", 3))
 
@@ -30,8 +48,8 @@ object MobileNoFormatter {
 
   def format(mobileNo: Option[String]): Option[String] = {
     mobileNo.flatMap { m =>
-      val mobile = m.trim
-      if (mobile.length == 0) {
+      val mobile = noPersianToEnglish(m.trim)
+      if (mobile.length == 0 || mobile.length > 10 || !isNumber(mobile)) {
         None
       } else {
         formats.map(x => (noPersianToEnglish(mobile).matches(x._1), x)).find(_._1).map(_._2._2).map(m.substring) match {
